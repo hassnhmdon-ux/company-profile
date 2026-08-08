8c104f321cc8da31d5d853664fb55e2b6dd301d5
hasan <hassnhmdon@gmail.com>
2026-08-08 21:29:12 +0300

    Balance catalog product sizes

--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,4 @@
-­r‡^Ñf¥–Ø¦{]lyÊ'vÃ®¶›­import { fireEvent, render, screen } from "@testing-library/react";
+­r‡^Ñf¥–Ø¦{^ìyÊ'vÃ®¶›­import { fireEvent, render, screen } from "@testing-library/react";
 import App from "./App";
 
 test("renders the Iraqi market homepage", () => {
@@ -22,9 +22,14 @@ test("renders the Iraqi market homepage", () => {
 
   fireEvent.click(screen.getByRole("button", { name: /Nuh'un Ankara logo/i }));
   expect(screen.getAllByAltText(/^Nuh'un Ankara product \d+$/i)).toHaveLength(22);
+  expect(screen.getByAltText("Nuh'un Ankara product 1")).toHaveClass("scale-[1.65]");
 
   fireEvent.click(screen.getByRole("button", { name: /Vitamin Enriched/i }));
   expect(screen.getAllByAltText(/^Vitamin Enriched product \d+$/i)).toHaveLength(10);
+
+  fireEvent.click(screen.getByRole("button", { name: /LOLO Rice logo/i }));
+  expect(screen.getByAltText("LOLO Rice product 1")).toHaveClass("scale-100");
+  expect(screen.getByAltText("LOLO Rice product 2")).toHaveClass("scale-[0.84]");
 
   fireEvent.click(screen.getByRole("button", { name: /Regal logo/i }));
   expect(screen.getAllByAltText(/^Regal product \d+$/i)).toHaveLength(14);
